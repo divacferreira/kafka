003d8820c41bb2b6249c6fb840c39e1ed5ba9a05
EDVALDO RODRIGUES FERREIRA <edvaldorodriguesferreira@MacBook-Pro-de-EDVALDO.local>
2023-06-09 12:36:22 -0300

    str-producer start

---
+++ str-producer/src/test/java/com/edvaldo/strproducer/StrProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edvaldo.strproducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StrProducerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
