c550b8250dee59bb83fd606c3ee2924c1dc218f3
EDVALDO RODRIGUES FERREIRA <edvaldorodriguesferreira@MacBook-Pro-de-EDVALDO.local>
2023-06-10 11:47:11 -0300

    docker-compose

---
+++ str-producer/src/main/java/com/edvaldo/strproducer/services/StringProducerService.java
@@ -0,0 +1,16 @@
+package com.edvaldo.strproducer.services;
+
+import lombok.RequiredArgsConstructor;
+import org.apache.kafka.common.protocol.types.Field;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+@RequiredArgsConstructor
+@Service
+public class StringProducerService {
+
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
+    public void sendMessage(String message){
+        kafkaTemplate.send("str-topic", message);
+    }
+}
