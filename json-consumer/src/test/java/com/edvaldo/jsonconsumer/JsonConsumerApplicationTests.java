5ca248e7b9d3aca903cfeac2d3d909656871323e
EDVALDO RODRIGUES FERREIRA <edvaldorodriguesferreira@MacBook-Pro-de-EDVALDO.local>
2023-06-15 09:56:26 -0300

    json-consumer

---
+++ json-consumer/src/test/java/com/edvaldo/jsonconsumer/JsonConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edvaldo.jsonconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsonConsumerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
